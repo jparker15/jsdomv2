220fcf4f8c7f68dd9c0e02c1673bf901c409d526
Jeremy Parker <jeremyparker.student@careerdevs.com>
2018-12-19 02:48:50 +0000

    used banner.nodeType, banner.nodeName, banner.hasChildNodes, banner.cloneNode(true)

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-var books = document.querySelectorAll('#book-list li .name');
-
-Array.from(books).forEach(function(book){
-    book.textContent += ' (book title)';
-});
-
-const bookList = document.querySelector('#book-list');
-//bookList.innerHTML = '<h2>Books and more books...</h2>';
-bookList.innerHTML += '<p>This is how you add HTML </p>';
+const banner = document.querySelector('#page-banner');
 
 
+console.log('#page-banner node type is', banner.nodeType);
+console.log('#page-banner node name is', banner.nodeName);
+console.log('#page-banner has child nodes', banner.hasChildNodes());
+
+
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
