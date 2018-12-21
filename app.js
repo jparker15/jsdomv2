e380758f492b4d0f9726de0f8d7b118eb171d136
Jeremy Parker <jeremyparker.student@careerdevs.com>
2018-12-21 01:42:01 +0000

    used .addEventListener

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-var btns = document.querySelectorAll('#book-list .delete');
-
-Array.from(btns).forEach(function(btn){
-  btn.addEventListener('click', function(e){
-    const li = e.target.parentElement;
+const list = document.querySelector('#book-list ul');
 
 
-    li.parentNode.removeChild(li);
-    
-  });
+
+//delete books
+list.addEventListener('click', function(e){
+  if(e.target.className == 'delete') {
+   const li =e.target.parentElement;
+   list.removeChild(li);
+  }
 });
 
-const link = document.querySelector('#page-banner a');
-
-link.addEventListener('click', function(e){
-  e.preventDefault();
-  console.log('navigation to', e.target.textContent, 'was prevented');
-});
 
