2552cd22e2c3f5083ed08d8c78645738ee6aedd1
Jeremy Parker <jeremyparker.student@careerdevs.com>
2018-12-21 01:17:58 +0000

    btn.addEventLister('click',...) , link.addEventListener('click'..)

--- app.js
+++ app.js
@@ -14,50 +14,6 @@ const link = document.querySelector('#page-banner a');
 
 link.addEventListener('click', function(e){
   e.preventDefault();
-  console.log('naviation to', e.target.textContent, 'was prevented');
+  console.log('navigation to', e.target.textContent, 'was prevented');
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const listItems = document.querySelectorAll('#book-list ul li');
-
-// Array.from(listItems).forEach(function(item){
-//   item.addEventListener('click', (e) => {
-
-//     const li = e.target.parentElement;
-//     console.log('child element to remove:', li);
-//     console.log('parent element to remove child from:', li.parentElement);
-//     li.parentNode.removeChild(li);
-
-//   });
-// });
-
-// // prevent default behaviour
-
-// const link = document.querySelector('#page-banner a');
-
-// link.addEventListener('click', function(e){
-//   e.preventDefault();
-//   console.log('Navigation to', e.target.textContent, 'was prevented');
-// });
