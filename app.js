351dcb893aeb1bc746bfb63e49980d9a960a1cae
Jeremy Parker <jeremyparker.student@careerdevs.com>
2018-12-30 21:21:36 +0000

    added list.addEventListener, if(e.target.classname, list.removeChild(li)

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const list = document.querySelector('#book-list ul');
 
 
-
-//delete books
+//delete books 
 list.addEventListener('click', function(e){
-  if(e.target.className == 'delete') {
-   const li =e.target.parentElement;
-   list.removeChild(li);
-  }
+  if(e.target.className == 'delete'){
+      const li = e.target.parentElement;
+      list.removeChild(li);
+  }  
+    
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
